89da4c2f41edc9a6001ed11271daacc756805c44
wuyulong <wuyulong@baiwang.com>
2023-10-18 18:25:09 +0800

    feat: build

--- auto-push.bat
+++ auto-push.bat
@@ -1,6 +1,6 @@
 REM 这是单行注释 进入目标文件夹
-REM 这是单行注释 cd D:/www.zichin.com
-REM 执行 git pull 命令来更新代码库
+:: 这是单行注释 cd D:/www.zichin.com
+:: 执行 git pull 命令来更新代码库
 git pull
 git add .
 git commit -m "feat: build"
